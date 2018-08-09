2e6f675c0839e70463959527d4a7a0a39b020b21
David Leeper <uscomics@protonmail.com>
2018-08-09 13:15:16 -0700

    Added GoldVenusClassic package

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace USComics_Player
+{
+    public class Health : MonoBehaviour
+    {
+        public static float MAX_HEALTH = 100;
+        public static int MAX_LIVES = 6;
+
+        public float health;
+        public int lives;
+
+        private GameObject healthBar;
+        private float initialHealthBarScale = 0.25f;
+        private GameObject life1;
+        private GameObject life2;
+        private GameObject life3;
+        private GameObject life4;
+        private GameObject life5;
+        private GameObject life6;
+
+        // Use this for initialization
+        void Start()
+        {
+            healthBar = GameObject.FindWithTag("HealthBar") as GameObject;
+            life1 = GameObject.FindWithTag("Life1") as GameObject;
+            life2 = GameObject.FindWithTag("Life2") as GameObject;
+            life3 = GameObject.FindWithTag("Life3") as GameObject;
+            life4 = GameObject.FindWithTag("Life4") as GameObject;
+            life5 = GameObject.FindWithTag("Life5") as GameObject;
+            life6 = GameObject.FindWithTag("Life6") as GameObject;
+
+            if (null == healthBar) { Debug.LogError("Health.Start: healthBar is null."); }
+            if (null == life1) { Debug.LogError("Health.Start: life1 is null."); }
+            if (null == life2) { Debug.LogError("Health.Start: life2 is null."); }
+            if (null == life3) { Debug.LogError("Health.Start: life3 is null."); }
+            if (null == life4) { Debug.LogError("Health.Start: life4 is null."); }
+            if (null == life5) { Debug.LogError("Health.Start: life5 is null."); }
+            if (null == life6) { Debug.LogError("Health.Start: life6 is null."); }
+
+            if (null == healthBar) { return; }
+            if (null == life1) { return; }
+            if (null == life2) { return; }
+            if (null == life3) { return; }
+            if (null == life4) { return; }
+            if (null == life5) { return; }
+            if (null == life6) { return; }
+
+            health = Health.MAX_HEALTH;
+            lives = 3;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float healthRemainingPrecentage = health / Health.MAX_HEALTH;
+            float healthBarPercentage = healthRemainingPrecentage * initialHealthBarScale;
+            Vector3 healthBarScale = healthBar.transform.localScale;
+            healthBar.transform.localScale = new Vector3(healthBarScale.x, healthBarPercentage, healthBarScale.z);
+
+            if (6 > lives) HideGameObject(life6); else ShowGameObject(life6);
+            if (5 > lives) HideGameObject(life5); else ShowGameObject(life5);
+            if (4 > lives) HideGameObject(life4); else ShowGameObject(life4);
+            if (3 > lives) HideGameObject(life3); else ShowGameObject(life3);
+            if (2 > lives) HideGameObject(life2); else ShowGameObject(life2);
+            if (1 > lives) HideGameObject(life1); else ShowGameObject(life1);
+
+        }
+
+        void HideGameObject(GameObject gameObject) { gameObject.SetActive(false); }
+        void ShowGameObject(GameObject gameObject) { gameObject.SetActive(true); }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using USComics_User_Input;
+
+public class PlayerController : MonoBehaviour {
+    public Direction direction = Direction.None;
+    public bool movementToggledOn = false;
+
+    private GameObject playerCharacter;
+    private Animator anim;
+    private GameObject movementPadIndicator;
+    private GameObject currentMovementTypePanel;
+    private MovementTypeMenu movementTypeMenuScript;
+    private Vector2 movementPadIndicatorOriginalPosition;
+    private GameObject healthPanel;
+    private Vector3 initialHelthPanelLocalPosition;
+    private Quaternion initialHelthPanelRotation;
+
+    private Vector3 previousVector = Vector3.zero;
+    private float speed = 1.0f;
+    private GameObject messageCanvas;
+
+    void Start () {
+        playerCharacter = GameObject.FindWithTag("PlayerCharacter") as GameObject;
+        if (null != playerCharacter) anim = playerCharacter.GetComponent<Animator>();
+        movementPadIndicator = GameObject.FindWithTag("MovementPadIndicator") as GameObject;
+        currentMovementTypePanel = GameObject.FindWithTag("CurrentMovementType") as GameObject;
+        if (null != currentMovementTypePanel) movementTypeMenuScript = currentMovementTypePanel.GetComponent<MovementTypeMenu>();
+        messageCanvas = GameObject.FindWithTag("MessageCanvas") as GameObject;
+        healthPanel = GameObject.FindWithTag("HealthGameObject") as GameObject;
+
+        if (null == playerCharacter) { Debug.LogError("PlayerController.Start: playerCharacter is null."); }
+        if (null == anim) { Debug.LogError("PlayerController.Start: anim is null."); }
+        if (null == movementPadIndicator) { Debug.LogError("PlayerController.Start: movementPadIndicator is null."); }
+        if (null == currentMovementTypePanel) { Debug.LogError("PlayerController.Start: currentMovementTypePanel is null."); }
+        if (null == movementTypeMenuScript) { Debug.LogError("PlayerController.Start: movementTypeMenuScript is null."); }
+        if (null == messageCanvas) { Debug.LogError("PlayerController.Start: messageCanvas is null."); }
+        healthPanel = GameObject.FindWithTag("HealthGameObject") as GameObject;
+
+        if (null == playerCharacter) { return; }
+        if (null == anim) { return; }
+        if (null == movementPadIndicator) { return; }
+        if (null == currentMovementTypePanel) { return; }
+        if (null == movementTypeMenuScript) { return; }
+        if (null == messageCanvas) { return; }
+        if (null == healthPanel) { return; }
+
+        movementPadIndicatorOriginalPosition = movementPadIndicator.transform.position;
+        initialHelthPanelLocalPosition = healthPanel.transform.localPosition;
+        initialHelthPanelRotation = healthPanel.transform.rotation;
+    }
+
+    void Update () {
+        MovementType movementType = Keyboard.GetMovementType();
+        if (MovementType.None != movementType)  {
+            speed = movementTypeMenuScript.SetMovementType(movementType);
+        } else {
+            speed = MovementSpeed.GetSpeed(MovementTypeMenu.currentMovementType);
+        }
+        Debug.Log("speed = " + speed);
+
+        Direction direction = Keyboard.GetDirection();
+        if (Direction.None == direction) { direction = MovementPad.GetDirection(); }
+
+        if (PlayerShouldStop(direction)) {
+            movementToggledOn = false;
+            anim.SetFloat("Speed", 0.0f);
+        } else if (PlayerShouldGo(direction)) {
+            setMovementTypeValues();
+            previousVector = MovementPad.ConvertDirectionToVector(direction, previousVector);
+            movementToggledOn = true;
+
+            playerCharacter.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(previousVector), 0.15F);
+            playerCharacter.transform.Translate (previousVector * speed * Time.deltaTime, Space.World);
+        }
+        if (Direction.None != direction) {
+            movementPadIndicator.transform.position = MovementPad.GetIndicatorPosition(direction, movementPadIndicatorOriginalPosition);
+            if (Direction.None != direction && Direction.Stop != direction) healthPanel.transform.localPosition = GetHealthPosition(direction);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        Keyboard.forceStop = true;
+    }
+
+    bool PlayerShouldStop(Direction direction) { return ((movementToggledOn) && (Direction.Stop == direction)); }
+
+    bool PlayerShouldGo(Direction direction) {
+       if (Direction.Stop == direction) return false;
+       if ((!movementToggledOn) && (Direction.None == direction)) return false;
+       return true;
+    }
+
+    void setMovementTypeValues() {
+        switch (MovementTypeMenu.currentMovementType)
+        {
+            case MovementType.Sneaking:
+                anim.SetBool("Sneak", true);
+                anim.SetFloat("Speed", MovementSpeed.GetSpeed(MovementType.Sneaking));
+                break;
+            case MovementType.Walking:
+                anim.SetBool("Sneak", false);
+                anim.SetFloat("Speed", MovementSpeed.GetSpeed(MovementType.Walking));
+                break;
+            case MovementType.Running:
+                anim.SetBool("Sneak", false);
+                anim.SetFloat("Speed", MovementSpeed.GetSpeed(MovementType.Running));
+                break;
+            case MovementType.Climbing:
+                anim.SetBool("Sneak", false);
+                anim.SetFloat("Speed", MovementSpeed.GetSpeed(MovementType.Climbing));
+                break;
+        }
+    }
+
+    Vector3 GetHealthPosition(Direction inDirection)
+    {
+        if (Direction.South == inDirection) return new Vector3(initialHelthPanelLocalPosition.x, initialHelthPanelLocalPosition.y, -initialHelthPanelLocalPosition.z);
+        if (Direction.SW == inDirection) return new Vector3(initialHelthPanelLocalPosition.x, initialHelthPanelLocalPosition.y, -initialHelthPanelLocalPosition.z);
+        if (Direction.SE == inDirection) return new Vector3(initialHelthPanelLocalPosition.x, initialHelthPanelLocalPosition.y, -initialHelthPanelLocalPosition.z);
+        return initialHelthPanelLocalPosition;
+    }
+}
